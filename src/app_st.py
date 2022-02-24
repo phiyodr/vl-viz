f19d1bbc251498633dea3a9cc3d2891601247f75
Albert Lattke <j9la0983@unibw.de>
2022-02-24 01:33:07 +0100

    Testversion 0.3.2 [WIP]
    - Dictionaries of 'groups' and 'types' were split up in own rows for each key
    - New Search function to search for multiple keywords in the new rows
    
    Please note that the Layout has to be redone in the future

--- src/app_st.py
+++ src/app_st.py
@@ -12,7 +12,7 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', uns
 
 st.title("Interactive Data-Explorer")
 st.write("""
-## This is Testversion v0.2
+## This is Testversion v0.3.2
 """)
 st.markdown("***")
 
@@ -26,29 +26,60 @@ base_df = pd.read_json(os.path.abspath(os.path.join(os.path.dirname(__file__), "
                        orient="index")
 base_df = base_df[columns]
 base_df['wordCount'] = 0
+base_df['global'] = base_df['local'] = base_df['detailed'] = base_df['semantic'] = base_df['structural'] = ''
 
 for i, row in base_df.iterrows():
-    base_df.at[i, 'wordCount'] = int(len(row['question'].split(" ")))
+    base_df.at[i, 'wordCount'] = len(row['question'].split(" "))
+    base_df.at[i, 'global'] = row['groups']['global'] if (
+            row['groups']['global'] is not None and bool(row['groups']['global']) is True) else 'No Entry'
+    base_df.at[i, 'local'] = row['groups']['local'] if (
+            row['groups']['local'] is not None and bool(row['groups']['local']) is True) else 'No Entry'
+    base_df.at[i, 'detailed'] = row['types']['detailed'] if (
+            row['types']['detailed'] is not None and bool(row['types']['detailed']) is True) else 'No Entry'
+    base_df.at[i, 'semantic'] = row['types']['semantic'] if (
+                row['types']['semantic'] is not None and bool(row['types']['semantic']) is True) else 'No Entry'
+    base_df.at[i, 'structural'] = row['types']['structural'] if (
+                row['types']['structural'] is not None and bool(row['types']['structural']) is True) else 'No Entry'
+
+base_df.drop(['groups', 'types'], axis=1, inplace=True)
+
 
 # Checkboxes
 
 selection = st.radio(label="Please select in which columns you want to search",
-                     options=('Questions', 'Answers', 'Both'))
+                     options=('Questions', 'Answers', 'Both', 'None'), index=3)
+
+keyphrase = st.text_input("Search for Keywords in Question and Answers")
+options = st.multiselect('Search additional columns', ['global', 'local', 'detailed', 'semantic', 'structural'])
+keywords = st.text_input("Enter Keywords")
+words = keywords.split()
+
 
 # Search by Keywords or phrases
 
-keys = st.text_input("Search for Keywords in Question and Answers")
-sorted_df = pd.DataFrame(columns=columns)
+sorted_df = pd.DataFrame()
 for index, row in base_df.iterrows():
-    if selection == "Questions":
-        if keys.lower() in row['question'].lower():
-            sorted_df = sorted_df.append(row)
-    if selection == "Answers":
-        if keys.lower() in keys.lower() in row['answer'].lower():
-            sorted_df = sorted_df.append(row)
-    if selection == "Both":
-        if keys.lower() in row['question'].lower() or keys.lower() in row['answer'].lower():
-            sorted_df = sorted_df.append(row)
+    #Search for Keywords
+    if selection != 'None':
+        if selection == "Questions":
+            if keyphrase.lower() in row['question'].lower():
+                sorted_df = sorted_df.append(row)
+        if selection == "Answers":
+            if keyphrase.lower() in keyphrase.lower() in row['answer'].lower():
+                sorted_df = sorted_df.append(row)
+        if selection == "Both":
+            if keyphrase.lower() in row['question'].lower() or keyphrase.lower() in row['answer'].lower():
+                sorted_df = sorted_df.append(row)
+
+    if options is not None:
+        for key in options:
+            for word in words:
+                if word.lower() in row[key].lower():
+                    if index not in sorted_df.index.values:
+                        sorted_df = sorted_df.append(row)
+
+if sorted_df.empty and selection == 'None':
+    sorted_df = base_df
 
 if not sorted_df.empty:
     displayed_df = sorted_df
